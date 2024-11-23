b5c7f9fa3999b6eded259b62b5ef0c9b1b9fc309
“Laurent <laurentlebarbier70@gmail.com>
2024-11-23 19:15:15 +0100

    Initial commit for exo_js-17-11-21

---
+++ cours.js
@@ -0,0 +1,8 @@
+function stephane(nombretapissouris, nombreblocnote, nompersonne) {
+    console.log('Stéphane dépose ${nombretapissouris} de tapis de souris et ${nombreblocnote} de bloc note à ${nompersonne}')
+}
+let nbtapis = 3;
+let nbbloc = 2;
+let name = 'Toilette'
+
+stephane(nombretapissouris, 2, nombreblocnote, 3)
